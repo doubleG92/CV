d12a63488dbc3c3a10f9d02f65ce1ead2d034b71
doubleG92 <1992gagikgalstyan@gmail.com>
2023-06-05 19:34:32 +0400

    add function onclick icon openning git page

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const gitHubIcon = document.getElementById('portfolio')
+const aboutIcon = document.getElementById('about')
+
+gitHubIcon.addEventListener('click', function() {
+    window.open('https://github.com/doubleG92')
+})
